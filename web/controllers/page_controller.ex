f9a533a09017093aa5bda74408e26ca13e7d40f4
Jason Draper <jason@drapergeek.com>
2015-12-31 16:08:19 -0500

    GOT IT WORKING ON THE WEB

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule HouseCardWeb.PageController do
   use HouseCardWeb.Web, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
+  def index(conn, params) do
+    case Map.fetch(params, "origin") do
+      {:ok, origin} ->
+        {:ok, trip} = HouseCard.lookup_trip(origin, "Washington, DC")
+        render conn, "index.html", trip: trip
+      :error ->
+        render conn, "index.html", trip: :error
+    end
   end
 end
